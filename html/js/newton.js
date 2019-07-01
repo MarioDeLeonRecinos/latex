a6ffc96de14aa76a0eeb450ff208a3d4dda662cb
Unknown <shulkmaster@gmail.com>
2019-07-01 00:05:39 -0600

    synch

--- html/js/newton.js
+++ html/js/newton.js
@@ -1,5 +1,4 @@
-const m = require('mathjs');
-
+/*
 const exp1 = m.parse('x+y*z + ln(2xy)');
 const exp2 = m.parse('2+x^y*z');
 const exp3 = m.parse('e^(2x) + z*y');
@@ -16,13 +15,27 @@ function jacobianMatrix(functions, scope) {
     return jacobian;
 }
 
-const mVector = [exp1, exp2, exp3];
-const jacob = jacobianMatrix(mVector, ['x', 'y', 'z']);
-console.log(jacob);
+//const mVector = [exp1, exp2, exp3];
+//const jacob = jacobianMatrix(mVector, ['x', 'y', 'z']);
+//console.log(jacob);
+*/
+document.getElementById('start').addEventListener('click', setVariables);
 
 function setVariables(){
-    var divlist = document.getElementById('eqpicker');
-    for(let x = 0; x < 4 ; x++){
-        divlist.appendChild(document.createc)
+    const nval = document.getElementById('nvalue').value;
+    const divlist = document.getElementById('equations');
+    console.log('xd');
+    for(let x = 0; x < nval ; x++){
+        let obj = document.createElement('input');
+        obj.type = "text";
+        obj.classList.add('form-control', 'form-rounded', 'eqsubmit');
+        divlist.appendChild(obj);
     }
+}
+
+function parseAll(){
+    const eqs = document.getElementsByClassName('eqsubmit');
+    eqs.forEach( e => {
+        e.value()
+    });
 }
